60fe1749192eb6651fae4c291bab36adc756a138
Yaroslav Fedoseev <yaroslav.site@mail.ru>
2022-12-01 02:56:22 +0700

    Непонятная фигня

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,12 +8,20 @@
             return z;
         }
 
+        private static void decs(ref int res, int x, int y)
+        {
+            res /= (x + y);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
             int res = func(a, b);
+            Console.WriteLine(res);
+
+            decs(ref res, a, b);
             Console.WriteLine(res);
         }
     }
