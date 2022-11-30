9828d867ef5e9e272cab39af45c3c186321545e5
Yaroslav Fedoseev <yaroslav.site@mail.ru>
2022-12-01 02:33:38 +0700

    Добавил ввод и вывод числа

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a);
         }
     }
 }
