e9d3985b3de686e1c00d87140dbd6137dc4c52da
Yaroslav Fedoseev <yaroslav.site@mail.ru>
2022-12-01 02:39:23 +0700

    Добавил функцию возводящую число в нужную степень

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -2,11 +2,19 @@
 {
     internal class Program
     {
+        private static void func(ref int z, int x, int y)
+        {
+            z = (int)Math.Pow(x, y);
+        }
+
         static void Main(string[] args)
         {
+            int z = 0;
             Console.WriteLine("Hello, World!");
             int a = int.Parse(Console.ReadLine());
-            Console.WriteLine(a);
+            int b = int.Parse(Console.ReadLine());
+            func(ref z, a, b);
+            Console.WriteLine(z);
         }
     }
 }
